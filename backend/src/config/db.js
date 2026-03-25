880ca54694a8304546196967a29e4366943542b1
R4fl1 <raflifndi@gmail.com>
2026-03-25 21:27:17 +0700

    second commit

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 export const connectDb = async () => {
   try {
     await mongoose.connect(process.env.MONGO_URI);
-    console.log("db connected");
+    console.log("MONGOO DB CONNECTED !!");
   } catch (error) {
     console.log("Error Connecting to MonggoDB", error);
     process.exit(1);
--- backend/src/server.js
+++ backend/src/server.js
@@ -1,19 +1,23 @@
 import express from "express";
 import noteRoutes from "./routes/notesRoutes.js";
-import {connectDb} from "./config/db.js";
-import dotenv from 'dotenv';
+import { connectDb } from "./config/db.js";
+import dotenv from "dotenv";
+import rateLimiter from "./middleware/rateLimit.js";
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5001;
 
-connectDb();
 //middleware
 app.use(express.json());
+app.use(rateLimiter);
 
 app.use("/api/notes", noteRoutes);
 
-app.listen(PORT, () => {
-  console.log("server started on PORT: 5001",PORT);
+connectDb().then(() => {
+  app.listen(PORT, () => {
+    console.log(`server started on PORT : ${PORT}`);
+  });
 });
+
 
