1135b59341b0076c813a9a11a4288cdbfbdcfd39
R4fl1 <raflifndi@gmail.com>
2026-03-31 02:45:36 +0700

    downgrade tailwind ke 3 dan daisyui ke 4

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,15 +2,22 @@ import { Route, Routes } from "react-router";
 import HomePage from "./pages/HomePage";
 import CreatePages from "./pages/CreatePages";
 import NoteDetail from "./pages/NoteDetail";
-import toast from "react-hot-toast";
 
 const App = () => {
   return (
-    <div data-theme="forest"> 
+    <div className="relative min-h-screen w-full">
+
+      {/* background */}
+      <div
+        className="absolute inset-0 -z-10 w-full
+      [background:radial-gradient(125%_125%_at_50%_10%,#000_60%,#00FF9D40_100%)]"
+      />
+
+      {/* content */}
       <Routes>
-        <Route path="/" element={<HomePage/>} />
-        <Route path="/create" element={<CreatePages/>} />
-        <Route path="/note/:id" element={<NoteDetail/>} />
+        <Route path="/" element={<HomePage />} />
+        <Route path="/create" element={<CreatePages />} />
+        <Route path="/note/:id" element={<NoteDetail />} />
       </Routes>
     </div>
   );
--- frontend/src/components/NoteCard.jsx
+++ frontend/src/components/NoteCard.jsx
@@ -1,8 +1,7 @@
 import { PenSquareIcon, Trash2Icon } from "lucide-react";
 
-import { Link } from "react-router"
+import { Link } from "react-router";
 import { formatDate } from "../lib/utils";
-
 
 const NoteCard = ({ note }) => {
   return (
@@ -14,19 +13,19 @@ const NoteCard = ({ note }) => {
       <div className="card-body">
         <h3 className="card-title text-base-content">{note.title}</h3>
         <p className="text-base-content/70 line-clamp-3">{note.content}</p>
-        <div className="card-actions justify-between items-center mt-4">
+        <div className="card-actions justify-between items-center mt-5">
           <span className="text-sm text-base-content/60">
             {formatDate(note.createdAt)}
           </span>
 
-          <div className="flex items-center gap-1">
-            <PenSquareIcon  className="size-4"/>
-            <button className="btn btn-ghost btn-xs text-red-500 ">
-              <Trash2Icon className="size-4 "/>
+          <div className="flex items-center gap-1.5">
+            <button className="btn btn-ghost btn-sm">
+              <PenSquareIcon className="size-4" />
+            </button>
+            <button className="btn btn-ghost btn-sm text-red-500 ">
+              <Trash2Icon className="size-4 " />
             </button>
           </div>
-
-
         </div>
       </div>
     </Link>
